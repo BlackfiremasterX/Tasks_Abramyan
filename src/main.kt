8c9c2db5dd5bc0abdd8086cd238c6c0a92ef145d
jake5529 <O_ivan_fomichev_O@mail.ru>
2021-01-26 17:28:28 +0700

    Begin20

--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,20 @@
+
+import kotlin.math.*
+
 fun main()
 {
     print("Добро пожаловать в методическое пособие на языке Kotlin по задачнику Абрамяна!\nТут представлены задачи по всем модулям.\n(Begin1 - b1) - Пример внутреннего синтаксиса для вызова задач из списка.")
-    print("На данный момент доступны задачи:\nBegin1\nBegin2")
-    Begin1()
-    Begin2()
+    print("На данный момент доступны задачи:\nBegin1\nBegin2\n")
+    print("Выберите задачу: "); var user_choise = readLine()
+    when(user_choise)
+    {
+        "b1" -> Begin1()
+        "b2" -> Begin2()
+        "b20" -> Begin20()
+        else -> println("Такой задачи нет!")
+
+    }
+
 //    Integer28()
 
 }
@@ -22,9 +33,24 @@ fun Begin2()
     var Sqare = a*a
     print("Площадь квадрата со стороной а = $a: $Sqare\nДемонстрация завершена!\n")
 }
+fun Begin20()
+{
+    var x1= readLine()!!.toDouble()
+    var x2= readLine()!!.toDouble()
+    var y1= readLine()!!.toDouble()
+    var y2= readLine()!!.toDouble()
+    var S = sqrt((x2-x1).pow(2)+(y2-y1).pow(2))
+    var Spow = ((x2-x1).pow(2)+(y2-y1).pow(2)).pow(0.5)
+    println("Результат: $S")
+    println("Результат SPOW: $Spow")
+
+//(/*выражение*/).pow(0.5) = sqrt(/*выражение*/)
+
+
+}
 ///
 
-
+/* Найти расстояние между двумя точками с заданными координатами(x1,y1) и (x2,y2) на плоскости. Расстояние вычисляется по формуле√(x2−x1)2+ (y2−y1)2*/
 
 //fun Integer28()
 //{
